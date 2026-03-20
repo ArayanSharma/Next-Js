fe42f55bba37daa7c5e819534a31fab5d5af85a4
ArayanSharma <arayanpandit11.com>
2026-03-20 12:26:22 +0530

    fatch  data for api in client server

---
+++ 02-next/src/app/components/User.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const User = async() => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users")
+    const data = await res.json()
+    console.log(data)
+
+  return (
+    <div className='flex flex-col justify-center gap-4 px-3 py-5 bg-amber-500'>
+         {data.map((user) => (
+       <h1 key={user.id} className='text-red-600 text-2xl font-bold w-full'>
+         {user.name}
+       </h1>
+       ))}
+    </div>
+  )
+}
+
+export default User;
